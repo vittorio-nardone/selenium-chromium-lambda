766087c9f0bc71c3fbf0588f0e4053e92a4ea0d6
Cyril Doussin <cyril@liquid-state.com>
2020-09-02 09:18:06 +1000

    Accept parameters for url, format and S3 key. Enable the creation of PDF files (simply embedding the PNG image for now)

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -13,21 +13,39 @@ def lambda_handler(event, context):
     logger.info('## ENVIRONMENT VARIABLES')
     logger.info(os.environ)
  
-    screenshot_file = "{}-{}".format(''.join(filter(str.isalpha, os.environ['URL'])), str(uuid.uuid4()))
+    screenshot_file = str(uuid.uuid4())
     driver = WebDriverScreenshot()
 
-    logger.info('Generate fixed height screenshot')
-    driver.save_screenshot(os.environ['URL'], '/tmp/{}-fixed.png'.format(screenshot_file), height=1024)
+    # logger.info('Generate fixed height screenshot')
+    # driver.save_screenshot(os.environ['URL'], '/tmp/{}-fixed.png'.format(screenshot_file), width=1245, height=1755)
 
-    logger.info('Generate full height screenshot')    
-    driver.save_screenshot(os.environ['URL'], '/tmp/{}-full.png'.format(screenshot_file))
+    png_file_name = '{}-full.png'.format(screenshot_file)
+    png_file_path = '/tmp/{}'.format(png_file_name)
+    pdf_file_name = png_file_name.replace('.png', '.pdf')
+    pdf_file_path = '/tmp/{}'.format(pdf_file_name)
+    logger.info('Generate full height screenshot')
+    url = event['url']
+    format = event['format']
+    key_path = event['s3_key']
+    driver.save_screenshot(url, png_file_path, width=1245, height=1755)
 
     driver.close()
 
+    if format == 'png':
+        s3.upload_file(png_file_path, 
+                    os.environ['BUCKET'],
+                    key_path)
+    if format == 'pdf':
+        from fpdf import FPDF
+        pdf = FPDF()
+        pdf.add_page()
+        pdf.image(png_file_path, 5, 5, 200)
+        pdf.output(pdf_file_path, 'F')
+        s3.upload_file(pdf_file_path, 
+                    os.environ['BUCKET'],
+                    key_path)
+
     ## Upload generated screenshot files to S3 bucket.
-    s3.upload_file('/tmp/{}-fixed.png'.format(screenshot_file), 
-                   os.environ['BUCKET'], 
-                   '{}/{}-fixed.png'.format(os.environ['DESTPATH'], screenshot_file))
-    s3.upload_file('/tmp/{}-full.png'.format(screenshot_file), 
-                   os.environ['BUCKET'], 
-                   '{}/{}-full.png'.format(os.environ['DESTPATH'], screenshot_file))
+    # s3.upload_file('/tmp/{}-fixed.png'.format(screenshot_file), 
+    #                os.environ['BUCKET'], 
+    #                '{}/{}-fixed.png'.format(os.environ['DESTPATH'], screenshot_file))
